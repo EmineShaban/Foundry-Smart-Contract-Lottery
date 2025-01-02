3f3b8b2f6f6cb879a8fe2a03a729eca3b27a30fc
Emine Shaban <emineshaban@icloud.com>
2025-01-02 10:47:13 +0200

    .

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -11,30 +11,38 @@ pragma solidity 0.8.19;
 contract Raffle {
     error Raffle__SendMoreToEnterRaffle();
 
-    uint256 private immutable i_EntranceFee;
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    event RaffleEntered (address indexed player)
+    event RaffleEntered (address indexed player);
 
-    constructor(uint256 entranceFee) {
-        i_EntranceFee = entranceFee;
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
-        if(msg.value < i_EntranceFee){
+    function enterRaffle() external payable {
+        if(msg.value < i_entranceFee){
             revert Raffle__SendMoreToEnterRaffle();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if((block.timestamp - s_lastTimeStamp) < i_interval){
+            revert();
+        }
+    }
 
     /**
      * Getter Functions
      */
 
     function getEntranceFee() external view returns (uint256) {
-        return i_EntranceFee;
+        return i_entranceFee;
     }
 }
