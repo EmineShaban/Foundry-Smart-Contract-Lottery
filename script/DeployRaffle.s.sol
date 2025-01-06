5b551dd44a2c13a036cbdc220c7d8ded5f126e19
Emine Shaban <emineshaban@icloud.com>
2025-01-06 12:08:27 +0200

    Subscribing to events

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,13 +4,27 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription} from "script/CreateSubscription.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
 
+
+
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
+
+
+
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        if (config.subscriptionId == 0){
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription.createSubscription(config.vrfCoordinator);
+        }
+        
+
+
         vm.startBroadcast();
         Raffle raffle = new Raffle(
             config.entranceFee,
